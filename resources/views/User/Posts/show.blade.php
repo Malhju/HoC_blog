db9242b77d1d32c32585c68f2ffbfe9de8598523
veroxy <v.lafaverges@gmail.com>
2018-07-23 18:17:37 +0200

    structure article en cours -- TOFIXED interpréter les retour à la ligne

--- resources/views/User/Posts/show.blade.php
+++ resources/views/User/Posts/show.blade.php
@@ -1,10 +1,18 @@
 @extends('layouts.app')
 @section('content')
     <h1 class="text-center">{{$Post->title}}</h1>
-    <div class="img-crop">
-        {{ Html::image($Post->photos()->first() ? $Post->photos()->first()->file : '','text alternatif') }}
-    </div>
-    <p>{{$Post->content}}</p>
+    <figure>
+        <div class="img-crop">
+            {{ Html::image($Post->photos()->first() ? $Post->photos()->first()->file : '','text alternatif') }}
+        </div>
+        <figcaption>
+            <p>
+                 {{$Post->content}}
+
+            </p>
+        </figcaption>
+    </figure>
+
     <p><em>créé par {{$Post->user->name}} le {{$Post->created_at}}</em></p> <br>
     <a href="{{ route('user.categories.show', $Post->category_id) }}">Retour aux articles de la catégorie {{$Post->category->name}}</a> <br>
     <a href="{{ route('user.categories.index') }}">Retour aux catégories</a> <br>
